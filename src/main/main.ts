61d6440c970f6ed1d879565d76d7555d2deb3edb
Adam <adamcsaraujo@gmail.com>
2022-08-09 22:29:22 -0300

    chore: add swagger and change main directory

---
+++ src/main/main.ts
@@ -0,0 +1,20 @@
+import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('/api/v1');
+
+  const config = new DocumentBuilder()
+    .setTitle('API Documentation')
+    .setDescription('COVID-19 API Documentation')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
+
+  app.enableCors();
+  await app.listen(3000);
+}
+bootstrap();
