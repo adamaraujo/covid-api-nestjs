754e583c47d0ed592a60ae5136567f71f6b66986
Adam <adamcsaraujo@gmail.com>
2022-08-16 23:12:32 -0300

    refactor: change axios client parameter

--- src/infra/axios/axios.client.ts
+++ src/infra/axios/axios.client.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { HttpClient } from "src/data/protocols";
 
 export class AxiosHttpClient implements HttpClient {
-  async get <T = any> (parametros: HttpClient.Parametros): Promise<T> {
-    return await axios.get(parametros.url);
+  async get <T = any> (url: HttpClient.Parametros): Promise<T> {
+    return await axios.get(url);
   }
 }
