17b250bb4dcfcc433e144ac2aab3d8c9e2f32aaa
Adam <adamcsaraujo@gmail.com>
2022-08-21 10:53:06 -0300

    feat: add charge indicator presentation controller

---
+++ src/presentation/controllers/indicador/carregar-indicador.ts
@@ -0,0 +1,33 @@
+import { CarregarPais, CarregarVacina } from "src/domain/usecases";
+import { serverError } from "src/presentation/helpers";
+import { Controller } from "src/presentation/protocols";
+
+export class CarregarIndicador implements Controller {
+  constructor(
+    private readonly carregarPais: CarregarPais,
+    private readonly carregarVacina: CarregarVacina
+  ) { }
+  
+  async lidar(): Promise<any> {
+    try {
+
+      const PAIS = 'Brazil';
+
+      const { pais, estados } = await this.carregarPais.carregar(PAIS);
+      const vacina = await this.carregarVacina.carregar(PAIS);
+
+      const indicador = {
+        pais: {
+          pais,
+          vacina
+        },
+        estados
+      }
+
+      return indicador;
+
+    } catch (error) {
+      return serverError(error);
+    }
+  }
+}
